36a4ea500b48aea38b2df81c8a8c729695504932
ziad benzerouala <ziadzerouala11@gmail.com>
2026-05-23 15:07:29 +0100

    fix: delivery_note exemple plus naturel

--- components/dashboard/integrations/custom-site-keys.tsx
+++ components/dashboard/integrations/custom-site-keys.tsx
@@ -242,7 +242,7 @@ Content-Type: application/json`}
   "address": "12 Rue de la Liberté",
   "total_selling_price": 250.00,
   "delivery_charge_to_customer": 25.00,
-  "delivery_note": "je serai dispo à demain 18h",
+  "delivery_note": "je serai dispo demain à 18h",
   "items": [
     {
       "product_id": "uuid-du-produit",
