9c3f08d8134488b06a0dc4c82eb75ef39ec5882d
ziad benzerouala <ziadzerouala11@gmail.com>
2026-06-09 23:21:58 +0100

    fix: mobile scale sur wrapper dashboard au lieu de body

--- app/(app)/layout-client.tsx
+++ app/(app)/layout-client.tsx
@@ -61,7 +61,7 @@ export default function AppLayoutClient({
   }
 
   return (
-    <div className="flex h-screen bg-background text-foreground">
+    <div className="flex h-screen bg-background text-foreground app-mobile-scale">
       <Sidebar />
       <OnboardingModal />
       <div className="flex-1 flex flex-col min-w-0">
