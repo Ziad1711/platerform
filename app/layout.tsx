fa796cd41d42171c722c1601ee642e678f36775e
ziad benzerouala <ziadzerouala11@gmail.com>
2026-06-09 23:04:24 +0100

    scale mobile global 0.9

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#1f2a23',
 }
 
